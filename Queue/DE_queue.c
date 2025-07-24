4b14d3a745bdb393a2ec69baacca6ff18f13faee
ATANU BYAPARI <work.atanubyapari.temp0@gmail.com>
2025-07-24 11:08:55 +0530

    Identifying error!

--- Queue/DE_queue.c
+++ Queue/DE_queue.c
@@ -1,3 +1,4 @@
 // queue using linked list, traversal, enqueue, dqueue, DE queue, double ended queue: both insertion and deletion can be done from both end i.e. front and rear end
 //  restricted input DE queue: Elements can't be added from front end
 //  restricted output DE queue: Elements can't be deleted from rear end
+//trying to understand the error!
